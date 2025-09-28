a588b2b0f1a1c71d47438d0ada16bfb0f92ea359
Karlabrojas <karlabrojasr2@gmail.com>
2025-09-27 23:53:49 -0600

    feat(db): agrega creacion de cliente y config de docker compose

---
+++ src/auth-service/db/db.ts
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Connection URL
+const url = process.env.URL_MONGODB;
+const dbName = "arq_sist_univ";
+
+let instance: MongoClient | null = null;
+
+async function connect() {
+  if (!instance) {
+    instance = new MongoClient(url!);
+    await instance.connect();
+    console.log("Conectado exitosamente a la base de datos");
+  }
+  return instance.db(dbName);
+}
+
+export { connect };
