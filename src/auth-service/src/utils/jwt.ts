a2cfb9ebc05094b43de94ec56ba89b0171a54a52
Arturocs160 <kronculd@outlook.com>
2025-09-28 20:27:19 -0600

    feat(jwt): implementar funciones para generar, verificar y decodificar tokens JWT

---
+++ src/auth-service/src/utils/jwt.ts
@@ -0,0 +1,26 @@
+import jwt, { Secret } from 'jsonwebtoken';
+
+const SECRET_KEY: Secret = process.env.JWT_SECRET!;
+
+export interface TokenPayload {
+  email: string;
+  password: string;
+  role?: string;
+}
+
+export function generateToken(payload: TokenPayload, expiresIn = 3600): string {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+
+export function verifyToken(token: string): TokenPayload | null {
+  try {
+    return jwt.verify(token, SECRET_KEY) as TokenPayload;
+  } catch (error) {
+    console.error('Token inválido:', error);
+    return null;
+  }
+}
+
+export function decodeToken(token: string): TokenPayload | null {
+  return jwt.decode(token) as TokenPayload | null;
+}
